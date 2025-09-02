1682a04df3fa27a7084604ca2bd589246e96e697
devark28 <e.shimwa1@alustudent.com>
2025-09-02 15:38:09 +0200

    Configure environment variables using dotenv and replace hardcoded MongoDB URI

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import mongoose from "mongoose";
-import itemsRouter from './items.router.js'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import itemsRouter from './items.router.js';
+
+dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+
 app.use('/items', itemsRouter);
 
-await mongoose.connect('mongodb://localhost/items_db');
+await mongoose.connect(process.env.MONGO_URI);
 
 app.listen(3000, () => {
   console.log('Server online...')
